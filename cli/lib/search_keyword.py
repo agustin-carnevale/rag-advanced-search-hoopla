9ef5841095a446fe3b36f8a4c407a18ffcdf6a74
Agustin Carnevale <agustinc.dev@gmail.com>
2025-10-27 01:23:45 -0300

    tokenize text for comparison

--- cli/lib/search_keyword.py
+++ cli/lib/search_keyword.py
@@ -9,14 +9,21 @@ import string
 def search_cmd(query: str, limit: int = DEFAULT_SEARCH_LIMIT) -> list[dict]:
   movies = load_movies()
   results = []
+  
+  query_tokens = tokenize_text(query)
   for movie in movies:
-    preprocessed_query = preprocess_text(query)
-    preprocessed_title = preprocess_text(movie["title"])
-    if preprocessed_query in preprocessed_title:
+    title_tokens = tokenize_text(movie["title"])
+    
+    if has_matching_token(query_tokens, title_tokens):
       results.append(movie)
       if len(results) >= limit:
-          break
+        break
+      
   return results
+
+def has_matching_token(query_tokens: list[str], title_tokens: list[str]) -> bool:
+  # Check if any token in list1 is a substring of any token in list2
+  return any(q_token in t_token for q_token in query_tokens for t_token in title_tokens)
 
 
 def preprocess_text(text: str) -> str:
@@ -39,3 +46,27 @@ def preprocess_text(text: str) -> str:
   text = text.translate(translation_table)
   
   return text
+
+def tokenize_text(text: str) -> list[str]:
+  """
+  Splits text into valid tokens. Pre-processing text, and removing empty tokens.
+  
+  Args:
+    text: The text to generate tokens from
+    
+  Returns:
+    List of tokens for that text
+  """
+  
+  # pre-process
+  text = preprocess_text(text)
+  
+  # split into workds
+  tokens = text.split(" ")
+  
+  # remove empty strings "", " ", etc
+  tokens = [s for s in tokens if s.strip() != ""]
+  
+  return tokens
+
+
