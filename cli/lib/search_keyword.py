c27d52a04d5d43685b073a1764f0d75af512c4a6
Agustin Carnevale <agustinc.dev@gmail.com>
2025-10-27 13:20:22 -0300

    filter stop words

--- cli/lib/search_keyword.py
+++ cli/lib/search_keyword.py
@@ -1,6 +1,7 @@
 from .search_utils import (
   DEFAULT_SEARCH_LIMIT,
   load_movies,
+  load_stop_words,
 )
 
 import string
@@ -8,11 +9,12 @@ import string
 
 def search_cmd(query: str, limit: int = DEFAULT_SEARCH_LIMIT) -> list[dict]:
   movies = load_movies()
+  stops_words = load_stop_words()
   results = []
   
-  query_tokens = tokenize_text(query)
+  query_tokens = tokenize_text(query, stops_words)
   for movie in movies:
-    title_tokens = tokenize_text(movie["title"])
+    title_tokens = tokenize_text(movie["title"], stops_words)
     
     if has_matching_token(query_tokens, title_tokens):
       results.append(movie)
@@ -47,7 +49,7 @@ def preprocess_text(text: str) -> str:
   
   return text
 
-def tokenize_text(text: str) -> list[str]:
+def tokenize_text(text: str, stops_words: list[str]) -> list[str]:
   """
   Splits text into valid tokens. Pre-processing text, and removing empty tokens.
   
@@ -65,7 +67,7 @@ def tokenize_text(text: str) -> list[str]:
   tokens = text.split(" ")
   
   # remove empty strings "", " ", etc
-  tokens = [s for s in tokens if s.strip() != ""]
+  tokens = [s for s in tokens if s.strip() != "" and s not in stops_words]
   
   return tokens
 
--- cli/lib/search_utils.py
+++ cli/lib/search_utils.py
@@ -5,9 +5,28 @@ DEFAULT_SEARCH_LIMIT = 5
 
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 DATA_PATH = os.path.join(PROJECT_ROOT, "data", "movies.json")
+STOP_WORDS_PATH = os.path.join(PROJECT_ROOT,"data", "stopwords.txt")
 
 
 def load_movies() -> list[dict]:
-  with open(DATA_PATH, "r") as f:
-    data = json.load(f)
-  return data["movies"]
+  try:
+    with open(DATA_PATH, "r") as f:
+      data = json.load(f)
+    return data["movies"]
+  except FileNotFoundError:
+      print(f"Error: The file '{DATA_PATH}' was not found.")
+  except Exception as e:
+      print(f"An error occurred: {e}")
+
+def load_stop_words() -> list[str]:
+  try:
+    with open(STOP_WORDS_PATH, "r") as f:
+      lines = f.readlines()    
+    # remove newline characters from each line
+    lines = [line.strip() for line in lines]
+    
+    return lines
+  except FileNotFoundError:
+      print(f"Error: The file '{STOP_WORDS_PATH}' was not found.")
+  except Exception as e:
+      print(f"An error occurred: {e}")
