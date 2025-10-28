2f63a650b100f84756683fad233ad734c6b42b60
Agustin Carnevale <agustinc.dev@gmail.com>
2025-10-28 17:58:48 -0300

    inverted index

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
 
 import argparse
-import json
+import sys
+from pathlib import Path
 
-from lib.search_keyword import search_cmd
+# Add project root to path to allow imports to work when running as script 
+project_root = Path(__file__).resolve().parent.parent
+if str(project_root) not in sys.path:
+  sys.path.insert(0, str(project_root))
+
+from cli.lib.inverted_index import InvertedIndex
+from cli.lib.search_keyword import search_cmd
 
 def main() -> None:
   parser = argparse.ArgumentParser(description="Keyword Search CLI")
@@ -12,6 +19,8 @@ def main() -> None:
   search_parser = subparsers.add_parser("search", help="Search movies using BM25")
   search_parser.add_argument("query", type=str, help="Search query")
 
+  subparsers.add_parser("build", help="build project inverted index")
+  
   args = parser.parse_args()
 
   match args.command:
@@ -22,6 +31,13 @@ def main() -> None:
       
       for i, movie in enumerate(results,1):
         print(f"{i}. {movie["title"]}")
+      pass
+    case "build":
+      inverted_index = InvertedIndex()
+      inverted_index.build()
+      inverted_index.save()
+      docs = inverted_index.get_documents("merida")
+      print(f"First document for token 'merida' = {docs[0]}")
       pass
     case _:
       parser.print_help()
